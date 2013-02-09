81faeadda33e4c2beeb3e7ab6cb27fa0112090d2
Noel <garcia.guimeras@gmail.com>
2013-02-09 16:27:49 -0500

    Very basic login and logout

--- djangounchained/basic/urls.py
+++ djangounchained/basic/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'djangounchained.basic.views.home', name='home'),
-    url(r'^home$', 'djangounchained.basic.views.home', name='home'),
-    url(r'^login$', 'djangounchained.basic.views.login', name='login'),
+    url(r'^$', 'djangounchained.basic.views.basic_home', name='home'),
+    url(r'^home$', 'djangounchained.basic.views.basic_home', name='home'),
+    url(r'^login$', 'djangounchained.basic.views.basic_login', name='login'),
+    url(r'^logout$', 'djangounchained.basic.views.basic_logout', name='logout'),    
 )
--- djangounchained/basic/views.py
+++ djangounchained/basic/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth import authenticate, login, logout
 
-def home(request):
-    params = {}
+def basic_home(request):
+    user = request.user
+
+    user_name = "visitor"
+    if user.is_authenticated():
+        user_name = user.first_name + " " + user.last_name + " (" + user.username + ")"
+        
+    params = {"user_name": user_name}
     return render_to_response("basic/hello.html", params)
     
-def login(request):
-    params = {}
+def basic_login(request):
+    user = request.user
+    
+    if user.is_authenticated():
+        return redirect("home")
+        
+    error_message = ""
+    user = authenticate(username="root", password="root")
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect("home")
+        else:
+            error_message = "Your account has been disabled!"
+    else:
+        error_message = "Your username and password were incorrect."
+    
+    params = {"error_message": error_message}
     return render_to_response("basic/login.html", params)
+    
+def basic_logout(request):
+    user = request.user
+    
+    if user.is_authenticated():
+        logout(request)
+        
+    return redirect("home")
